096753ac1c29dfa6445fc473e1ff0df847091ce3
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2026-01-26 14:55:32 +0100

    Update init-models.js

--- models/init-models.js
+++ models/init-models.js
@@ -3,16 +3,19 @@ var _empresa = require("./empresa");
 var _proveedor = require("./proveedor");
 
 function initModels(sequelize) {
-  var empresa = _empresa(sequelize, DataTypes);
-  var proveedor = _proveedor(sequelize, DataTypes);
+    var empresa = _empresa(sequelize, DataTypes);
+    var proveedor = _proveedor(sequelize, DataTypes);
 
-  proveedor.belongsTo(empresa, { as: "id_empresa_empresa", foreignKey: "id_empresa"});
-  empresa.hasMany(proveedor, { as: "proveedor", foreignKey: "id_empresa"});
+    proveedor.belongsTo(empresa, { as: "id_empresa_empresa", foreignKey: "id_empresa"});
+    empresa.hasMany(proveedor, { as: "proveedor", foreignKey: "id_empresa"});
 
-  return {
-    empresa,
-    proveedor
-  };
+    empresa.sync({ alter: true })
+    proveedor.sync({ alter: true })
+
+    return {
+        empresa,
+        proveedor
+    };
 }
 module.exports = initModels;
 module.exports.initModels = initModels;
